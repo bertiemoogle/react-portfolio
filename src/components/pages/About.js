23e1de8338846fa4cc2f2b95a8535e25d3196450
bertiemoogle <bertiemoogle@gmail.com>
2023-03-07 11:17:22 +0000

    About page completed.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,24 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function About() {
   return (
     <div>
       <h1 className="headerFont">About Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+        <div class="row aboutRow">
+          <img src={require("./images/aGRsaGd_d.jpg")} className="thinkImg" alt="A person thinking" />
+            <div class="col-lg-5 col-sm-12">
+                <p>Having been ill for a long time with anxiety and depression I decided
+                    I needed to do something constructive to occupy my time. I first studied for, and graduated with, an
+                    honours degree in literature, having had vague ideas about becoming a teacher. After that I
+                    came across the Python programming language and discovered that I love writing code. The opportunity
+                    to attend this front end web development bootcamp arose, which I have eagerly taken up. The world
+                    wide web is still in its infancy, and I don't want to merely see what happens to it - I really want
+                    to be a part of it.
+                </p>
+            </div>
+            
+        </div>
     </div>
   );
 }
