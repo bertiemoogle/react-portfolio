2fac0641bcdf8ece0d058881bb9403956948ad68
bertiemoogle <bertiemoogle@gmail.com>
2023-03-07 20:51:04 +0000

    Projects page layout finally working.

--- src/components/pages/Card.jsx
+++ src/components/pages/Card.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-function DisplayCard() {
+function DisplayCard(props) {
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+        <div className="card">
+            <div className="cardBody">
+                <img src={props.img} className="testImg" alt="test"/>
+                <h2 className="cardTitle">{props.title}</h2>
+                <p className="cardDescription">{props.description}</p>
+            </div>
+            <button className="cardBtn">GitHub Repo</button>
+        </div>
     );
   }
   
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,22 +1,44 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import Card from 'react-bootstrap/Card';
 import DisplayCard from "./Card";
 
 function Projects() {
   return (
-    <div>
+    <div className="container">
       <h1 className="headerFont">Projects</h1>
       <div className="row firstRow">
-        <DisplayCard />
-        <DisplayCard />
-        <DisplayCard />
+      <div className="col-lg-4">
+        <DisplayCard 
+          img="https://smaller-pictures.appspot.com/images/dreamstime_xxl_65780868_small.jpg"
+          title="Repo One"
+          description="First Repo"/></div>
+      <div className="col-lg-4">
+        <DisplayCard 
+          img="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png"
+          title="Repo One"
+          description="First Repo"/></div>
+      <div className="col-lg-4">
+        <DisplayCard 
+          img="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png"
+          title="Repo One"
+          description="First Repo"/></div>
       </div>
       <div className="row secondRow">
-        <DisplayCard />
-        <DisplayCard />
-        <DisplayCard />
+      <div className="col-lg-4">
+        <DisplayCard 
+          img="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png"
+          title="Repo One"
+          description="First Repo"/></div>
+      <div className="col-lg-4">
+        <DisplayCard 
+          img="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png"
+          title="Repo One"
+          description="First Repo"/></div>
+      <div className="col-lg-4">
+        <DisplayCard 
+          img="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png"
+          title="Repo One"
+          description="First Repo"/></div>
       </div>  
     </div>
   );
